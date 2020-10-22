24f40de25c47ef8907b9374af3aee4d8e42b902c
Rupal Nigam <rupaln@Rupals-MacBook-Pro.local>
2020-10-22 17:21:06 -0600

    comment out imshow

--- yolo_video.py
+++ yolo_video.py
@@ -16,7 +16,7 @@ def detect_img(yolo):
             continue
         else:
             r_image = yolo.detect_image(image)
-            r_image.show()
+            #r_image.show()
     yolo.close_session()
     return results
 
